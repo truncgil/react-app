e62be02f874be1fcdcdb7cf1c12a591435d00230
truncgil <umitreva@gmail.com>
2023-02-13 17:55:11 +0300

    modified react for truncgil

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         Welcome Truncgil Technology
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://truncgi.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Go to Truncgil
         </a>
       </header>
     </div>
