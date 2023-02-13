24cb730818020c0b766be1a4032f22c496bef842
truncgil <umitreva@gmail.com>
2023-02-13 19:01:33 +0300

    add sample fetch truncgil finance api

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {  useState, useEffect } from 'react';
 
 function App() {
+
+  const [currencies, setCurrencies] = useState([]);
+  useEffect(() => {
+    fetch('https://finans.truncgil.com/v4/today.json')
+      .then((response) => response.json())
+      .then((data) => {
+       console.log(data);
+       console.log(Object.entries(data));
+        setCurrencies(Object.entries(data));
+        
+      })
+      .catch((err) => {
+        console.log(err.message);
+     });
+  });
+
   return (
+  
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -17,6 +35,30 @@ function App() {
         >
           Go to Truncgil
         </a>
+        <table>
+          <thead>
+            <tr>
+              <td>Tür</td>
+              <td>Alış</td>
+              <td>Satış</td>
+            </tr>
+          </thead>
+          <tbody>
+            
+          {Array.isArray(currencies) 
+          ? currencies.map((currency, index) => {
+            return (
+              <tr>
+                <td>{currency[0]}</td>
+                <td>{currency[1].Buying}</td>
+                <td>{currency[1].Selling}</td>
+              </tr>
+            )
+            
+          }) : null }
+          </tbody>
+        </table>
+        
       </header>
     </div>
   );
